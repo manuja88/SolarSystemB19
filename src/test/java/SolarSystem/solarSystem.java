4128a940652679a1dd2f9a2dbc59e69f72756b17
manuja88 <manujagrgthapa@gmail.com>
2024-06-09 13:53:10 -0400

    done with feature sun

--- src/test/java/SolarSystem/solarSystem.java
+++ src/test/java/SolarSystem/solarSystem.java
@@ -1,12 +1,17 @@
 package SolarSystem;
 
 public class solarSystem {
-
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             SolarSystemBase solar = new SolarSystemBase();
             solar.sun="1";
             solar.planet="2";
             solar.star="2";
             System.out.println("the solar system is ready");
+
+            //...
+        featureSun xros= new featureSun();
+        xros.color="pink";
+        xros.temp="24324kv";
+        xros.size="0km";
         }
 }
